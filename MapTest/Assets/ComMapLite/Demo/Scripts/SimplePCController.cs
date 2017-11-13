43f54a9a69cae0ab0a54b83a7a5e05d41260a15c
deveuper <249068538@qq.com>
2017-11-13 18:37:18 +0800

    首次上传MapTest工程

---
+++ MapTest/Assets/ComMapLite/Demo/Scripts/SimplePCController.cs
@@ -0,0 +1,158 @@
+﻿using UnityEngine;
+using System.Collections;
+namespace xurun
+{
+    public class SimplePCController : MonoBehaviour
+    {
+        // Move speed
+        public float speed = 5.0f;
+        private float walkSpeed;
+        [Range(5, 100)]
+        //Run speed
+        public float runSpeed = 100f;
+        private bool isRunning = false;
+
+		public Transform m_FirstCamera;
+		public Transform m_ThridCamera;
+
+		private Transform m_Camera;
+        private Transform m_Character;
+        //Rotate the Y Axis use mouse X
+        private Quaternion m_CharacterTargetRot;
+        //Rotate the X Axis for Camera use mouse Y
+        private Quaternion m_CameraTargetRot;
+        public float XSensitivity = 2f;
+        public float YSensitivity = 2f;
+
+        private float smoothTime = 5f;
+        public bool smoothMouse = false;
+
+        private Rigidbody rigidBody;
+
+        public MouseMoveRotationModel mouseModelRotate = MouseMoveRotationModel.MMORPGGameModel;
+        private void InitCamera()
+        {
+			if (m_Camera == null) {
+				switch (mouseModelRotate) {
+				case MouseMoveRotationModel.FPSGameModel:
+					m_Camera = m_FirstCamera;
+					m_ThridCamera.gameObject.SetActive (false);
+					break;
+				case MouseMoveRotationModel.MMORPGGameModel:
+					m_Camera = m_ThridCamera;
+					m_FirstCamera.gameObject.SetActive (false);
+					break;
+				default:
+					break;
+				}
+				m_Camera = Camera.main.transform;
+			}
+			if (m_Character == null) 
+            {
+				m_Character = transform;
+			}
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+            InitCamera();
+            //rigidBody = GetComponent<Rigidbody>();
+            walkSpeed = speed;
+            m_CharacterTargetRot = transform.localRotation;//Get the rotation
+            m_CameraTargetRot = m_Camera.localRotation;//Get the rotation
+        }
+        void Update()
+        {
+            InputControl();
+            MouseRotationModel();
+        }
+        private void MouseRotationModel()
+        {
+            switch (mouseModelRotate)
+            {
+                case MouseMoveRotationModel.FPSGameModel:
+                    MouseLook();
+                    Cursor.lockState = CursorLockMode.Locked;
+                    Cursor.visible = false;
+                    break;
+                case MouseMoveRotationModel.MMORPGGameModel:
+                    if (Input.GetMouseButton(1))
+                    {
+                        MouseLook();
+                        Cursor.lockState = CursorLockMode.Locked;
+                        Cursor.visible = false;
+                    }
+                    else
+                    {
+                        Cursor.lockState = CursorLockMode.None;
+                        Cursor.visible = true;
+                    }
+
+                    break;
+                default:
+                    break;
+            }
+
+        }
+        private void MouseLook()
+        {
+            float yRot = Input.GetAxis("Mouse X") * XSensitivity;
+            float xRot = Input.GetAxis("Mouse Y") * YSensitivity;
+            m_CharacterTargetRot *= Quaternion.Euler(0f, yRot, 0f);
+            m_CameraTargetRot *= Quaternion.Euler(-xRot, 0f, 0f);
+            if (smoothMouse)
+            {
+                m_Character.localRotation = Quaternion.Slerp(m_Character.localRotation, m_CharacterTargetRot, smoothTime * Time.deltaTime);
+                m_Camera.localRotation = Quaternion.Slerp(m_Camera.localRotation, m_CameraTargetRot, smoothTime * Time.deltaTime);
+            }
+            else
+            {
+                m_Character.localRotation = m_CharacterTargetRot;
+                m_Camera.localRotation = m_CameraTargetRot;
+            }
+        }
+        private void InputControl()
+        {
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Translate(new Vector3(speed * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.A))
+            {
+                transform.Translate(new Vector3(-speed * Time.deltaTime, 0, 0));
+            }
+            if (Input.GetKey(KeyCode.E))
+            {
+                transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+            }
+            if (Input.GetKey(KeyCode.Q) || Input.GetKey(KeyCode.Space))
+            {
+                transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+            }
+            if (Input.GetKey(KeyCode.S))
+            {
+                transform.Translate(new Vector3(0, 0, -speed * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                transform.Translate(new Vector3(0, 0, speed * Time.deltaTime));
+            }
+            if (Input.GetKey(KeyCode.LeftShift))
+            {
+                isRunning = true;
+            }
+            else
+            {
+                isRunning = false;
+            }
+            speed = isRunning ? runSpeed : walkSpeed;
+        }
+
+    }
+    public enum MouseMoveRotationModel
+    {
+        FPSGameModel,//use mouse to rotate player
+        MMORPGGameModel//down right mouse button and move mouse to rotate player
+    }
+}
---
+++ MapTest/Assets/ComMapLite/Scripts/ComMapSystem.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ComMap
+{
+    public class ComMapSystem : MonoBehaviour
+    {
+        //Target Player
+        public GameObject TargetPlayer;
+        //Arrow
+        public GameObject TargetArrow;
+        //using the target to get the value
+        public GameObject TerrainToShow;
+
+        public bool isTerrain = true;
+
+        public GameObject TargetPoint;
+        //using the cast map
+        public GameObject MapToShow;
+        //Get the Map Mask layer
+        public GameObject MapMask;
+        [HideInInspector]
+        public MeshFilterInfo TargetInfo;
+        [HideInInspector]
+        public MeshFilterInfo MapInfo;
+        [Space(1)]
+
+        [Range(0.01f, 2f)]
+        public float MapScaleValue = 1f;
+        public Vector3 TerrainPlanePosition;
+        private float ratioX;
+        private float ratioZ;
+
+        private float TerrainX;
+        private float TerrainZ;
+
+        private float MapX;
+        private float MapZ;
+
+        private float ScaleInit;
+
+        private void Awake()
+        {
+            GetTargetHeightAndWidth();
+            GetMapInfo();
+        }
+        // Use this for initialization
+        void Start()
+        {
+            TerrainPlanePosition = TerrainToShow.transform.position;
+        }
+        Vector3 TargetPlayerPosition;
+        // Update is called once per frame
+        void Update()
+        {
+            TargetPlayerPosition = TargetPlayer.transform.position;
+            PutPointIntoMap();
+            MapScaleAndTerrianPos();
+        }
+        // Get the target width and height for cast ready
+        private void GetTargetHeightAndWidth()
+        {
+            if (isTerrain)
+            {
+                GetTerrainOrPlaneInfo();
+            }
+            else
+            {
+                GetTargetInfo();
+            }
+        }
+
+        private void GetTargetInfo()
+        {
+            TargetInfo.X = TargetPoint.transform.position.x;
+            TargetInfo.Z = TargetPoint.transform.position.z;
+            TerrainX = TargetInfo.X;
+            TerrainZ = TargetInfo.Z;
+        }
+
+        private void GetTerrainOrPlaneInfo()
+        {
+            TargetInfo.X = TerrainToShow.GetComponent<MeshFilter>().mesh.bounds.size.x * TerrainToShow.transform.localScale.x;
+            TargetInfo.Z = TerrainToShow.GetComponent<MeshFilter>().mesh.bounds.size.z * TerrainToShow.transform.localScale.z;
+            TerrainX = TargetInfo.X / 2;
+            //Debug.Log(TerrainX + " , " + TerrainZ);
+            TerrainZ = TargetInfo.Z / 2;
+
+        }
+        // Get UI map image width and height value
+        private void GetMapInfo()
+        {
+            MapInfo.X = MapToShow.GetComponent<RectTransform>().rect.width;
+            MapInfo.Z = MapToShow.GetComponent<RectTransform>().rect.height;
+            MapX = MapInfo.X / 2;
+            MapZ = MapInfo.Z / 2;
+        }
+        // Calculate the ratio for the player position on terrain
+        private void Calculate()
+        {
+            if (isTerrain)
+            {
+                ratioX = (TargetPlayerPosition.x - TerrainToShow.transform.position.x) / TerrainX;
+                ratioZ = (TargetPlayerPosition.z - TerrainToShow.transform.position.z) / TerrainZ;
+            }
+            else
+            {
+                ratioX = (TargetPlayerPosition.x - TerrainToShow.transform.position.x) / Mathf.Abs(TargetPoint.transform.position.x - TerrainToShow.transform.position.x);
+                ratioZ = (TargetPlayerPosition.z - TerrainToShow.transform.position.z) / Mathf.Abs(TargetPoint.transform.position.x - TerrainToShow.transform.position.x);
+            }
+            //Debug.Log(TargetPlayerPosition.x +","+ratioX + "," + TerrainX);
+            //Debug.Log(ratioZ);
+        }
+        private void PutPointIntoMap()
+        {
+            Calculate();
+            Debug.Log(MapScaleValue);
+            float tempX = ratioX * MapX * MapScaleValue;
+            float tempZ = ratioZ * MapZ * MapScaleValue;
+            //Make the map move down,it sames like the playerArrow move up on the map
+            MapToShow.GetComponent<RectTransform>().localPosition = new Vector3(-tempX, -tempZ, 0);
+            //+ MapMask.GetComponent<RectTransform>().position;
+            ArrowRotation();
+        }
+        // Arrow look at player's forward direction
+        private void ArrowRotation()
+        {
+            TargetArrow.GetComponent<RectTransform>().rotation = Quaternion.Euler(new Vector3(0, 0, -TargetPlayer.transform.localEulerAngles.y - 180));
+        }
+
+        private void MapScaleAndTerrianPos()
+        {
+            MapToShow.GetComponent<RectTransform>().localScale = new Vector3(MapScaleValue, MapScaleValue, MapScaleValue);
+            TerrainToShow.transform.position = TerrainPlanePosition.x != 0 ? TerrainPlanePosition : TerrainToShow.transform.position;
+        }
+
+        #region Next Version
+        private void Bigger()
+        {
+            if (MapToShow.GetComponent<RectTransform>().localScale.x <= ScaleInit + 1)//make the biggest scale less than ScaleInit+1
+            {
+                MapToShow.GetComponent<RectTransform>().localScale.Set(MapToShow.GetComponent<RectTransform>().localScale.x + 0.2f,
+                    MapToShow.GetComponent<RectTransform>().localScale.x + 0.2f, MapToShow.GetComponent<RectTransform>().localScale.x + 0.2f);
+            }
+        }
+        private void Small()
+        {
+            if (MapToShow.GetComponent<RectTransform>().localScale.x >= ScaleInit - 1)//make the biggest scale more than ScaleInit+1
+            {
+                MapToShow.GetComponent<RectTransform>().localScale.Set(MapToShow.GetComponent<RectTransform>().localScale.x - 0.2f,
+                MapToShow.GetComponent<RectTransform>().localScale.x - 0.2f, MapToShow.GetComponent<RectTransform>().localScale.x - 0.2f);
+            }
+        }
+        #endregion
+    }
+    public struct MeshFilterInfo
+    {
+        public float X;
+        public float Y;
+        public float Z;
+    }
+
+}
